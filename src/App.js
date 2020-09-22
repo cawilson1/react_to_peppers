b24b4eac030ae398e7fffff8c57f64b0a6ace3c3
cawilson1 <cawilson1@g.cofc.edu>
2020-09-22 09:55:38 -0400

    created a card for first item in pepper db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import peppersDb from "./fakeDb/pepperDb";
 
 function App() {
-  return <div>Pepper page</div>;
+  console.log(peppersDb);
+  return (
+    <div>
+      <img src={peppersDb[0].pic} alt="pepper" width="200 px" />
+      <div>{peppersDb[0].id}</div>
+      <h1>{peppersDb[0].name}</h1>
+      <h3>{peppersDb[0].species}</h3>
+
+      <div>{peppersDb[0].flavor}</div>
+      <div>{peppersDb[0].scoville}</div>
+      <div>{peppersDb[0].size}</div>
+      <div>{peppersDb[0].growthTimeMonths}</div>
+      <div>{peppersDb[0].color}</div>
+    </div>
+  );
 }
 
 export default App;
