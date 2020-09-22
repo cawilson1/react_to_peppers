77f11d1ad09a2e256b95ed87138635d0d5efe9c8
cawilson1 <cawilson1@g.cofc.edu>
2020-09-22 09:45:25 -0400

    inital commit, create pepper db

---
+++ src/fakeDb/pepperDb.js
@@ -0,0 +1,76 @@
+const peppersDb = [
+  {
+    name: "Golden Cayenne",
+    scoville: 40000, //heat
+    pic:
+      "https://edge.bonnieplants.com/www/tiny/uploads/20200810205412/golden-cayenne-pepper.jpg",
+    flavor: "spicy, sweet, apple-y",
+    color: "yellow",
+    species: "capsicum annuum",
+    growthTimeMonths: 1,
+    size: 4, //1-10 (1 smallest)
+    id: 1
+  },
+  {
+    name: "Bishops Crown",
+    scoville: 10000, //heat
+    pic:
+      "https://www.chilipeppermadness.com/wp-content/uploads/2019/08/Bishops-Crown-Peppers.jpg",
+    flavor: "spicy, fruity",
+    color: "red",
+    species: "Capsicum baccatum",
+    growthTimeMonths: 1,
+    size: 4, //1-10 (1 smallest)
+    id: 2
+  },
+  {
+    name: "Tabasco",
+    scoville: 50000, //heat
+    pic:
+      "https://i.pinimg.com/originals/51/24/21/51242165ed07bd39ecb0d45319d72d35.jpg",
+    flavor: "spicy, smoky, citrusy",
+    color: "yellow, red",
+    species: "Capsicum frutescens",
+    growthTimeMonths: 1,
+    size: 2, //1-10 (1 smallest)
+    id: 3
+  },
+  {
+    name: "Serrano",
+    scoville: 17000, //heat
+    pic:
+      "https://www.thespruceeats.com/thmb/s90Vt-yDdfJeVKrHnsGCop7LhNg=/1333x1000/smart/filters:no_upscale()/GettyImages-930279012-e1d17f3f2e14473db877437ce57b8f5f.jpg",
+    flavor: "spicy fresh",
+    color: "green or red",
+    species: "capsicum annuum",
+    growthTimeMonths: 3,
+    size: 5,
+    id: 4
+  },
+  {
+    name: "Poblano",
+    scoville: 1500,
+    pic:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Poblano_Pepper.jpg/330px-Poblano_Pepper.jpg",
+    flavor: "smoky",
+    color: "green",
+    species: "capsicum annuum",
+    growthTimeMonths: 6,
+    size: 8,
+    id: 5
+  },
+  {
+    name: "Thai Hot Ornamental Pepper",
+    scoville: 75000,
+    pic:
+      "https://edge.bonnieplants.com/www/tiny/uploads/20200810205134/thai-hot-pepper.jpg",
+    flavor: "Hot",
+    color: "red",
+    species: "capsicum annuum",
+    growthTimeMonths: 3,
+    size: 2,
+    id: 6
+  }
+];
+
+export default peppersDb;
